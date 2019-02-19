259b5ed17d1a779ee1c48d5f844371e6ada2997d
Igor Skripnik <mailspibls@gmail.com>
2019-02-19 20:06:57 +0200

    Initial Commit

---
+++ ToDoApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApplication
+//
+//  Created by Igor Skripnik on 19.02.2019.
+//  Copyright © 2019 garik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
