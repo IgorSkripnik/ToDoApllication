1f2dfab540a9fcdde208de7bf724df6ff50d37bb
Igor Skripnik <mailspibls@gmail.com>
2019-02-25 13:35:12 +0200

    first commit

---
+++ ToDoApplication/CoreData/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  ToDoApplication
+//
+//  Created by Igor Skripnik on 21.02.2019.
+//  Copyright © 2019 garik. All rights reserved.
+//
+
+import Foundation
---
+++ ToDoApplication/VC/AddToDoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddToDoViewController.swift
+//  ToDoApplication
+//
+//  Created by Igor Skripnik on 20.02.2019.
+//  Copyright © 2019 garik. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
